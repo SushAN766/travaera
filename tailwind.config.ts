f6ef0902f8590b656e8dd1f8b69a436e471a6628
SushAN766 <sushanthbs766@gmail.com>
2025-05-07 17:24:58 +0530

    vulnerabilities remain unresolved

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
     "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
+    "./src/**/*.{html,js,jsx,ts,tsx,vue}",
   ],
   prefix: "",
   theme: {
